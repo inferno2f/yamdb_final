1398c3c7e0496a910af91fb3789dd64327fca671
Vlad Nikitin <nikitinv91@gmail.com>
2022-02-16 14:38:56 +0300

    drf yasg added

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,13 +1,33 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
+]
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="YAMDB API",
+      default_version='v1',
+      description="Документация для приложения YAMDB API",
+      contact=openapi.Contact(email="nikitinv91@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+   url(r'^swagger(?P<format>\.json|\.yaml)$', 
+       schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
+       name='schema-swagger-ui'),
+   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+       name='schema-redoc'),
 ]
